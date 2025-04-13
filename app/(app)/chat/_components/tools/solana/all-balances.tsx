dd3b94b480debce2d6b0bda20866742f41b3d4d4
trongdev <trongdev@trongdevs-MacBook-Pro.local>
2025-04-13 17:54:05 +0700

    fix(build): fix build do ai do code ngu

--- app/(app)/chat/_components/tools/solana/all-balances.tsx
+++ app/(app)/chat/_components/tools/solana/all-balances.tsx
@@ -1,48 +1,44 @@
-import React from 'react'
+import React from "react";
 
-import ToolCard from '../tool-card';
+import ToolCard from "../tool-card";
 
-import type { ToolInvocation } from 'ai';
-import type { CardanoAllBalancesResultType, NewCardanoAllBalancesResultType } from '@/ai';
-import { TokenBalance } from '../utils';
+import type { NewCardanoAllBalancesResultType } from "@/ai";
+import type { ToolInvocation } from "ai";
+import { TokenBalance } from "../utils";
 
 interface Props {
-    tool: ToolInvocation,
-    prevToolAgent?: string,
+  tool: ToolInvocation;
+  prevToolAgent?: string;
 }
 
 const AllBalances: React.FC<Props> = ({ tool, prevToolAgent }) => {
-
-
-                
-    return (
-        <ToolCard
-            tool={tool}
-            loadingText={`Getting All Balances...`}
-            result={{
-                heading: (result: NewCardanoAllBalancesResultType) => result.body
-                    ? `Fetched All Balances`
-                    : `Failed to fetch balances`,
-                body: (result: NewCardanoAllBalancesResultType) => result.body
-                    ? (
-                        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
-                            {result.body.balances.amount.map((balance) => (
-                                
-                                <TokenBalance
-                                    key={balance.unit}
-                                    token={balance.ticker}
-                                    balance={Number(balance.quantity)}
-                                    logoURI={balance.logo!}
-                                    name={balance.name}
-                                />
-                            ))}
-                        </div>
-                    )
-                    : "No balance found"
-            }}
-            prevToolAgent={prevToolAgent}
-        />
-    )
-}
+  return (
+    <ToolCard
+      tool={tool}
+      loadingText={`Getting All Balances...`}
+      result={{
+        heading: (result: NewCardanoAllBalancesResultType) =>
+          result.body ? `Fetched All Balances` : `Failed to fetch balances`,
+        body: (result: NewCardanoAllBalancesResultType) =>
+          result.body ? (
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+              {result.body.balances.amount.map((balance) => (
+                <TokenBalance
+                  key={balance.unit}
+                  token={balance.ticker}
+                  balance={Number(balance.quantity)}
+                  logoURI={balance.logo!}
+                  name={balance.name}
+                />
+              ))}
+            </div>
+          ) : (
+            "No balance found"
+          ),
+      }}
+      prevToolAgent={prevToolAgent}
+    />
+  );
+};
 
 export default AllBalances;
