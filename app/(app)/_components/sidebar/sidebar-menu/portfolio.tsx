7fa68d77a1283c4167a3d458c057a848d62a2bd4
TrungyuD <shawnkool10@gmail.com>
2025-06-08 12:33:17 +0700

    fix: change address stake of portfolio

--- app/(app)/_components/sidebar/sidebar-menu/portfolio.tsx
+++ app/(app)/_components/sidebar/sidebar-menu/portfolio.tsx
@@ -11,17 +11,14 @@ import { usePathname } from 'next/navigation';
 import { useSpidexCoreContext } from '@/app/_contexts';
 import { SidebarMenuButton, SidebarMenuItem } from '@/components/ui';
 import Image from 'next/image';
-import { useCardano } from '@cardano-foundation/cardano-connect-with-wallet';
 
 const Portfolio: React.FC = () => {
   const pathname = usePathname();
-  const { stakeAddress } = useCardano();
   const { auth } = useSpidexCoreContext();
-
-  if (!auth?.user?.walletAddress) return null;
+  if (!auth?.user?.stakeAddress) return null;
   const isActive = pathname?.includes('/portfolio');
   return (
-    <Link href={`/portfolio/${stakeAddress}`}>
+    <Link href={`/portfolio/${auth?.user?.stakeAddress}`}>
       <SidebarMenuItem>
         <SidebarMenuButton isActive={isActive}>
           <h1 className="flex items-center gap-2 font-semibold">
