206e096a365e6dacba70373a42c1dc9a0accd63a
TrungyuD <shawnkool10@gmail.com>
2025-06-23 16:54:33 +0700

    fix: report bug icon

--- app/(app)/_components/report-bug/index.tsx
+++ app/(app)/_components/report-bug/index.tsx
@@ -1,49 +1,81 @@
 'use client';
-
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from '@/components/ui';
 import React from 'react';
 
 const ReportBug = () => {
+  const [isHovered, setIsHovered] = React.useState(false);
   return (
     <div
-      className="absolute bottom-0 right-5 cursor-pointer"
+      className="absolute bottom-0 right-5 cursor-pointer flex items-center gap-2 "
       onClick={() => {
         window.open(
           'https://spidex-ai.gitbook.io/spidex-ai-docs/bug-bounty-program',
           '_blank'
         );
       }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      <img src="/icons/report-bug.svg" alt="Report Bug" />
+
+      <div
+        className={`flex gap-2 items-center ${isHovered ? 'border-2 border-[#009EFF] rounded-full' : ''}`}
+      >
+        <div
+          className={`px-3 py-2 pl-6  rounded-lg text-[#009EFF] font-medium text-sm whitespace-nowrap transition-all duration-300 ${
+            isHovered
+              ? 'opacity-100 translate-x-0'
+              : 'opacity-0 translate-x-4 pointer-events-none'
+          }`}
+        >
+          Report a bug
+        </div>
+
+  
+        <img
+          src="/icons/report-bug-icon.svg"
+          alt="Report Bug"
+          className="block"
+        />
+      </div>
     </div>
   );
 };
 
 export const ReportBugIcon = () => {
+  const [isHovered, setIsHovered] = React.useState(false);
   return (
     <div
-      className="absolute bottom-4 right-5 cursor-pointer"
-      onClick={() => {
-        window.open(
-          'https://spidex-ai.gitbook.io/spidex-ai-docs/bug-bounty-program',
-          '_blank'
-        );
-      }}
+    className="absolute bottom-4 right-5 cursor-pointer flex items-center gap-2 "
+    onClick={() => {
+      window.open(
+        'https://spidex-ai.gitbook.io/spidex-ai-docs/bug-bounty-program',
+        '_blank'
+      );
+    }}
+    onMouseEnter={() => setIsHovered(true)}
+    onMouseLeave={() => setIsHovered(false)}
+  >
+
+    <div
+      className={`flex gap-2 items-center ${isHovered ? 'border-2 border-[#009EFF] rounded-full' : ''}`}
     >
-      <TooltipProvider>
-        <Tooltip delayDuration={0}>
-          <TooltipTrigger>
-            <img src="/icons/report-bug-icon.svg" alt="Report Bug" />
-          </TooltipTrigger>
-          <TooltipContent>Report a bug</TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+      <div
+        className={`px-3 py-2 pl-6  rounded-lg text-[#009EFF] font-medium text-sm whitespace-nowrap transition-all duration-300 ${
+          isHovered
+            ? 'opacity-100 translate-x-0'
+            : 'opacity-0 translate-x-4 pointer-events-none'
+        }`}
+      >
+        Report a bug
+      </div>
+
+
+      <img
+        src="/icons/report-bug-icon.svg"
+        alt="Report Bug"
+        className="block"
+      />
     </div>
+  </div>
   );
 };
 
