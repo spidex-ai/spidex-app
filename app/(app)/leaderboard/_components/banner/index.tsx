3ca9699934bff912bd3f5637d3f8f62db87b1ca1
TrungyuD <shawnkool10@gmail.com>
2025-07-02 20:11:03 +0700

    fix: add cursor to leaderboard banner

--- app/(app)/leaderboard/_components/banner/index.tsx
+++ app/(app)/leaderboard/_components/banner/index.tsx
@@ -2,6 +2,7 @@
 
 import { GradientBorderButton, GradientButton } from '@/components/ui';
 import { TextGradient } from '@/components/ui/text';
+import Image from 'next/image';
 import React from 'react';
 
 const Banner: React.FC = () => {
@@ -13,24 +14,35 @@ const Banner: React.FC = () => {
           <TextGradient className="text-3xl font-meidum">
             Referral Race
           </TextGradient>
-          <div className="text-white">
-            Flex Your Rank. Invite, Win, Repeat!
-          </div>
+          <div className="text-white">Flex Your Rank. Invite, Win, Repeat!</div>
         </div>
         <div className="absolute top-1/3 right-[100px]">
-            <div className='flex gap-2'>
+          <div className="flex gap-2">
+            <div>
+              <GradientBorderButton className="!bg-black">
+                Rewards: 500 $ADA
+              </GradientBorderButton>
+            </div>
+            <div className="relative">
               <div>
-                <GradientBorderButton className='!bg-black'>Rewards: 500 $ADA</GradientBorderButton>
+                <GradientButton
+                  className="font-medium px-4 md:px-6 py-1 md:py-2 rounded-3xl"
+                  onClick={() => {
+                    window.open(
+                      'https://spidex-ai.gitbook.io/spidex-ai-docs/referral-race',
+                      '_blank'
+                    );
+                  }}
+                >
+                  Join now
+                </GradientButton>
               </div>
-              <div> 
-                <GradientButton className='font-medium px-4 md:px-6 py-1 md:py-2 rounded-3xl' onClick={() => {
-                  window.open('https://spidex-ai.gitbook.io/spidex-ai-docs/referral-race', '_blank');
-                }}>Join now</GradientButton>
+              <div className="absolute bottom-[-10px] right-[-10px]">
+                <Image src="/icons/arrow-cursor.svg" alt="arrow-right" width={40} height={40} />
               </div>
             </div>
-
+          </div>
         </div>
-
       </div>
     </div>
   );
