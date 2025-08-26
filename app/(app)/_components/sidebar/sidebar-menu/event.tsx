87600390149bd5a47b46884a43d386be066e6a1a
TrungyuD <shawnkool10@gmail.com>
2025-08-26 19:54:58 +0700

    feat: add event coming soon

--- app/(app)/_components/sidebar/sidebar-menu/event.tsx
+++ app/(app)/_components/sidebar/sidebar-menu/event.tsx
@@ -1,29 +1,31 @@
-
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-import { SidebarMenuItem, SidebarMenuButton, useSidebar } from '@/components/ui';
+import {
+  SidebarMenuItem,
+  SidebarMenuButton,
+  useSidebar,
+  Badge,
+} from '@/components/ui';
 import Image from 'next/image';
 
-
 const Event: React.FC = () => {
-    const pathname = usePathname();
-    const { isMobile, setOpenMobile } = useSidebar();
-    const isActive = pathname?.includes('/event');
-    const href = '/event';
+  const pathname = usePathname();
+  const { isMobile, setOpenMobile } = useSidebar();
+  const isActive = pathname?.includes('/event');
+  const href =
+    process.env.NEXT_PUBLIC_ENVIRONMENT !== 'production' ? '/event' : '/#';
 
-    const handleClick = () => {
-        if (isMobile) {
-          setOpenMobile(false);
-        }
-      };
-  
+  const handleClick = () => {
+    if (isMobile) {
+      setOpenMobile(false);
+    }
+  };
+
   return (
     <Link href={href} onClick={handleClick}>
       <SidebarMenuItem>
-        <SidebarMenuButton
-          isActive={pathname?.includes('/event') ?? false}
-        >
+        <SidebarMenuButton isActive={pathname?.includes('/event') ?? false}>
           <div className="flex items-center gap-2 w-full">
             <h1 className="flex items-center gap-2 font-semibold">
               {isActive ? (
@@ -44,6 +46,12 @@ const Event: React.FC = () => {
                 />
               )}
               <span className="pt-1">Event</span>
+              <Badge
+                variant="destructive"
+                className="text-[10px] h-5 w-fit px-1 rounded-md"
+              >
+                Coming Soon
+              </Badge>
             </h1>
           </div>
         </SidebarMenuButton>
